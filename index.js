8783a50da47335b68b4911726681bf69b997bd32
Zhao Tian <zhaotian.org@gmail.com>
2023-05-17 15:35:34 +0800

    ListDatabases and CreateCollection Functions.

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ run().catch(console.error); // Run the async function
 
 async function run() {
     try {
-            // Connect the client to the server	(optional starting in v4.7)
+            // Connect the client to the server	(optional starting in v4.7) -Faru
             await client.connect();
 
-            // Send a ping to confirm a successful connection
+            // Confirm a successful connection
             await client.db("admin");
             console.log("You successfully connected to MongoDB!");
+
+            // Make the appropriate DB calls
+            await listDatabases(client);
+
+            // Create a document
+            await createListing(client, {
+                username: "zt",
+                password: "321abc"
+            });
+
 
         } 
             catch (e) {
@@ -26,4 +36,15 @@ async function run() {
                 }
 }
 
+// Print the names of all available databases
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));    //  list all the databases
+}
 
+// Create a collection
+async function createListing(client, newListing){
+    const result = await client.db("BENR2423").collection("Users").insertOne(newListing);
+    console.log(`New data with id: ${result.insertedId} is created!`);     // Show that a new data created
+}
